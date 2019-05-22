560169aa50957f0c188e5318a3f16025c0fc70d8
kr3at0 <kr3at0@riseup.net>
2019-05-22 18:08:07 +0300

    add tag, filter and search functionality to the todo list

--- client/src/app/todos/todos.component.ts
+++ client/src/app/todos/todos.component.ts
@@ -13,6 +13,8 @@ export class TodosComponent implements OnInit {
     done: [],
     tags: {}
   };
+  appliedFilters: string[] = [];
+  searchTerm: string = '';
 
   constructor(private todoService: TodoService) { }
 
@@ -22,17 +24,56 @@ export class TodosComponent implements OnInit {
 
   getTodos(): void {
     this.todoService.getTodos()
-      .subscribe(todos => this.todos = todos);
+      .subscribe(todos => {
+        this.todos = { ...todos }
+        if (this.appliedFilters.length) this.applyFilters();
+        if (this.searchTerm) this.searchTodos();
+      });
   }
 
   markAsDone(todo: Todo) {
     this.todoService.moveToDone(todo)
-      .subscribe((todos => this.todos = todos));
+      .subscribe(todos => {
+        this.todos = { ...todos }
+        if (this.appliedFilters.length) this.applyFilters();
+        if (this.searchTerm) this.searchTodos();
+      });
   }
 
   markAsTodo(todo: Todo) {
     this.todoService.moveToToDo(todo)
-      .subscribe((todos => this.todos = todos));
+      .subscribe(todos => {
+        this.todos = { ...todos }
+        if (this.appliedFilters.length) this.applyFilters();
+        if (this.searchTerm) this.searchTodos();
+      });
   }
 
+  applyFilters(): void {
+    this.todos.todo = this.todos.todo.filter(todo => this.appliedFilters.every(t => todo.tags.includes(t)));
+    this.todos.done = this.todos.done.filter(todo => this.appliedFilters.every(t => todo.tags.includes(t)));
+  } 
+
+  filterTodos(tag: string): void {
+    if(this.appliedFilters.includes(tag)) {
+      this.appliedFilters[this.appliedFilters.indexOf(tag)] = this.appliedFilters[this.appliedFilters.length - 1];
+      this.appliedFilters.pop();
+      this.getTodos();
+    } else {
+      this.appliedFilters.push(tag);
+    }
+    console.log(this.appliedFilters);
+    this.applyFilters();
+  }
+
+  showAllTodos(): void {
+    this.appliedFilters = [];
+    this.getTodos();
+  }
+
+  searchTodos(): void {
+    console.log(this.searchTerm);
+    this.todos.todo = this.todos.todo.filter(todo => todo.content.indexOf(this.searchTerm) >= 0);
+    this.todos.done = this.todos.done.filter(todo => todo.content.indexOf(this.searchTerm) >= 0);
+  }
 }
